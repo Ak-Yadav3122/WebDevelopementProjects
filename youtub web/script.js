9372fd6fd81136ea3ce4548d9755629caa2a3b4d
Ak-Yadav3122 <aichchhik12@gmail.com>
2023-10-24 15:36:28 +0530

    This project is youtube clone

---
+++ youtub web/script.js
@@ -0,0 +1,11 @@
+// code for onclick effect
+var menuIcon = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container")
+}
+// end of onclick effect
